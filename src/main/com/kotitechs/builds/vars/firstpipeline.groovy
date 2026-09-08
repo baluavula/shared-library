159a08c331b52823d6487c58b4e817f750e56cc9
baluavula <159177615+baluavula@users.noreply.github.com>
2026-09-08 12:18:07 +0530

    commit final changes

--- src/main/com/kotitechs/builds/vars/firstpipeline.groovy
+++ src/main/com/kotitechs/builds/vars/firstpipeline.groovy
@@ -1,4 +1,6 @@
-import  com.kotitechs.builds.calculator
+import com.kotitechs.builds.calculator
+
+library('com.prasadtechz.slb')
 
 def call(Map pipelineparams){
     Calculator calc = new Calculator(this)
